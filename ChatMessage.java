205fc08652aa4785dd38eaf5e6c976d3b09d5a7e
Lathika17 <lathikamca17@gmail.com>
2025-12-04 19:52:52 +0530

    Create ChatMessage.java

---
+++ ChatMessage.java
@@ -0,0 +1,27 @@
+package multithreading;
+import java.io.*;
+
+public class ChatMessage implements Serializable {
+
+	// The different types of message sent by the Client
+	// WHOISIN to receive the list of the users connected
+	// MESSAGE an ordinary text message
+	// LOGOUT to disconnect from the Server
+	static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2;
+	private int type;
+	private String message;
+	
+	// constructor
+	ChatMessage(int type, String message) {
+		this.type = type;
+		this.message = message;
+	}
+	
+	int getType() {
+		return type;
+	}
+
+	String getMessage() {
+		return message;
+	}
+}
